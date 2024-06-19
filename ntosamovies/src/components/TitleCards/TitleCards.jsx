5108bd6f7d754a91ba95d8c56fac7c8b2130882e
samuel <masuvhe3@gmail.com>
2024-06-19 03:42:45 +0200

    complete

--- ntosamovies/src/components/TitleCards/TitleCards.jsx
+++ ntosamovies/src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+
+const TitleCards = ({title, category}) => {  
+
+    const cardsRef = useRef ();
+
+const handleWheel = (event) => {
+        event.preventDefault();
+        cardsRef.current.scrollLeft += event.deltaY;
+}
+
+useEffect (()=>{
+    cardsRef.current.addEventListener('wheel', handleWheel);
+},[])
+
   return (
-    <div className='titlecards'>
-      
+    <div className='title-cards'>
+      <h2>{title?title:"Trending on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index)=>{
+            return <div className="card" key={index}>
+                <img src={card.image} alt={card.title} />
+                <p>{card.name}</p>
+            </div>
+        })}
+      </div>
     </div>
   )
 }
--- ntosamovies/src/pages/Home/Home.jsx
+++ ntosamovies/src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
 
 const Home = () => {
   return (
@@ -21,7 +22,14 @@ const Home = () => {
               <button className='btn'><img src={play_icon} alt=""/>Watch Now</button>
               <button className='btn dark-btn'><img src={info_icon} alt=""/>More Info</button>
             </div>
+            <TitleCards/>
         </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"}/>
+        <TitleCards title={"Only on Netflix"}/>
+        <TitleCards title={"Upcoming"}/>
+        <TitleCards title={"Top Picks for You"}/>
       </div>
     </div>
   )
