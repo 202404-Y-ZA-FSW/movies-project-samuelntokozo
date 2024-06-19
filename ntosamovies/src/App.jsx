91750b2712414a4782d7d7906d21cf8d15fd4965
samuel <masuvhe3@gmail.com>
2024-06-19 15:29:54 +0200

    complete

--- ntosamovies/src/App.jsx
+++ ntosamovies/src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import Home from './pages/Home/Home'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from  './pages/Login/Login'
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
 
--- ntosamovies/src/components/TitleCards/TitleCards.jsx
+++ ntosamovies/src/components/TitleCards/TitleCards.jsx
@@ -5,7 +5,7 @@ import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title, category}) => {  
 
-    const cardsRef = useRef ();
+    const cardsRef = useRef (null);
 
 const handleWheel = (event) => {
         event.preventDefault();
@@ -20,7 +20,7 @@ useEffect (()=>{
     <div className='title-cards'>
       <h2>{title?title:"Trending on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index)=>{
+        {cards_data.map((card, index)=>{ 
             return <div className="card" key={index}>
                 <img src={card.image} alt={card.title} />
                 <p>{card.name}</p>
