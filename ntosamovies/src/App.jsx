975ac98233755eee9b1f7dfc8f3f4dec098663a3
Thitevhelwi Samuel Masuvhe <31856641+samuelthis@users.noreply.github.com>
2024-06-22 14:20:26 +0200

    Navbar

--- ntosamovies/src/App.jsx
+++ ntosamovies/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from  './pages/Login/Login'
 import Player from './pages/Player/Player';
-import Footer from './components/Footer/footer';
 import Actors from './pages/Actors/Actors';
 
 
