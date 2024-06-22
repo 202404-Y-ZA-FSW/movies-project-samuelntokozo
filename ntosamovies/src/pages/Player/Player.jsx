8837a5c48607616dbce5347993dde6711b57394f
Thitevhelwi Samuel Masuvhe <31856641+samuelthis@users.noreply.github.com>
2024-06-22 18:13:08 +0200

    addedarrow

--- ntosamovies/src/pages/Player/Player.jsx
+++ ntosamovies/src/pages/Player/Player.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
 
   const {id} = useParams();
+  const navigate = useNavigate();
 
   const[apiData, setApiData] = useState({
     name: "",
@@ -31,7 +32,7 @@ const Player = () => {
 
   return (
     <div className='player'>
-        <img src={back_arrow_icon} alt="" />
+        <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
         <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`}
         title='trailer' frameBorder='0' allowFullScreen></iframe>
         <div className="player-info">
