20fbe5befa9dd52a804cd505fa144b196dbbade5
Ntokozo <nkosintokozot@gmail.com>
2024-06-22 18:56:35 +0200

    updated

--- ntosamovies/src/components/TitleCards/TitleCards.jsx
+++ ntosamovies/src/components/TitleCards/TitleCards.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 // import cards_data from '../../assets/cards/Cards_data'
-
+import {Link} from 'react-router-dom'
 
 const TitleCards = ({title, category}) => {  
     const [apiData, setApiData] =useState([]);
@@ -31,6 +31,7 @@ useEffect (()=>{
 
   cardsRef.current.addEventListener('wheel', handleWheel);
 },[])
+   
 
   return (
     <div className='title-cards'>
@@ -38,8 +39,10 @@ useEffect (()=>{
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index)=>{
             return <div className="card" key={index}>
+              <Link to={`/player/${card.id}`} className="card" key={index}>
                 <img src={ `https://image.tmdb.org/t/p/w500` +card.backdrop_path} alt={card.title} />
                 <p>{card.original_title}</p>
+                </Link>
             </div>
         })}
       </div>
