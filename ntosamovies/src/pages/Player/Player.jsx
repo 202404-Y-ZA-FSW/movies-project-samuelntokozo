ad3cd75fcf3e7d2ec74b7060937d5b8cd8ce96e6
Thitevhelwi Samuel Masuvhe <31856641+samuelthis@users.noreply.github.com>
2024-06-22 17:35:17 +0200

    updated

--- ntosamovies/src/pages/Player/Player.jsx
+++ ntosamovies/src/pages/Player/Player.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams();
 
   const[apiData, setApiData] = useState({
     name: "",
@@ -19,7 +22,7 @@ const Player = () => {
   };
 
   useEffect(()=>{
-    fetch('https://api.themoviedb.org/3/movie/1022789/videos?language=en-US', options)
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
     .then(response => response.json())
     .then(response => setApiData(response.results[0]))
     .catch(err => console.error(err));
@@ -32,7 +35,7 @@ const Player = () => {
         <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`}
         title='trailer' frameBorder='0' allowFullScreen></iframe>
         <div className="player-info">
-          <p>{apiData.published_at}</p>
+          <p>{apiData.published_at.slice(0,10)}</p>
           <p>{apiData.name}</p>
           <p>{apiData.type}</p>
         </div>
