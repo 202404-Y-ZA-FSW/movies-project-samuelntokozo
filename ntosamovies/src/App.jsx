d584db288a79f37391ea439e4bf9ebd0eb78bef1
Thitevhelwi Samuel Masuvhe <31856641+samuelthis@users.noreply.github.com>
2024-06-22 14:15:24 +0200

    nav

--- ntosamovies/src/App.jsx
+++ ntosamovies/src/App.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Login from  './pages/Login/Login'
-import Player from './pages/Player/Player';
-import Footer from './components/Footer/footer';
-import Actors from './pages/Actors/Actors';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'; 
+import Login from './pages/Login/Login'; 
+import Player from './pages/Player/Player'; 
+import Actors from './pages/Actors/Actors'; 
 
 const App = () => {
   return (
-    
-    // <Router>
+   // <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/player/:id' element={<Player/>}/>
+        <Route path='/player/:id' element={<Player />} />
         <Route path='/actors' element={<Actors />} />
       </Routes>
-      // <Footer/>
-   /* </Router> */
-  
+    // </Router>
   );
 };
 
 export default App;
+
+
+
 
 
