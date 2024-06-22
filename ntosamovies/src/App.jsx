c75183009b39c19d3a4b071182b5d9d24d1193e6
Thitevhelwi Samuel Masuvhe <31856641+samuelthis@users.noreply.github.com>
2024-06-22 15:18:39 +0200

    nav

--- ntosamovies/src/App.jsx
+++ ntosamovies/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'; 
+import Login from './pages/Login/Login'; 
+import Player from './pages/Player/Player';
 import Actors from './pages/Actors/Actors';
+import SingleActorsPage from './pages/Actors/SingleActorsPage';
 
 
 const App = () => {
   return (
-
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
@@ -13,7 +16,6 @@ const App = () => {
         <Route path='/actors' element={<Actors />} />
         <Route path='/actors/:id' element={<SingleActorsPage />} /> 
       </Routes>
-
   );
 };
 
