b3b4b04641d9e712946011abad4868317d74f5d6
Ntokozo <nkosintokozot@gmail.com>
2024-06-23 02:34:45 +0200

    nav

--- ntosamovies/src/components/Navbar/Navbar.jsx
+++ ntosamovies/src/components/Navbar/Navbar.jsx
@@ -7,20 +7,44 @@ import { Link } from 'react-router-dom';
 
 
 
+
 const Navbar = () => {
+  const [genres, setGenres] = useState([]);
+  const [movieOptions, setMovieOptions] = useState([
+    { label: 'Top Rated', value: 'top_rated' },
+    { label: 'Popular', value: 'popular' },
+    { label: 'Latest', value: 'latest' },
+    { label: 'Now Playing', value: 'now_playing' },
+    { label: 'Upcoming', value: 'upcoming' },
+  ]);
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=d7f883f6d380f7e3c2ad35c7dab44528')
+      .then(response => {
+        setGenres(response.data.genres);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value;
+    // implement search logic here, e.g. make API call to search endpoint
+    console.log(`Searching for ${searchTerm}`);
+  };
+
   return (
     <div className='navbar'>
       <div className="navbar-left">
         <img src={logo} alt="" />
         <ul>
           <li>Home</li>
-          <li><Link>Movies</Link></li>
-          <li> <Link>TV Shows</Link></li>
+          <li> <Link className="link-a" to='/Movies'>Movies</Link></li>
+          <li> <Link className="link-a" to='/TV Shows'>TV Shows</Link></li>
           <li>Genres</li>
-       <li><Link className="link-a" to='/actors'>Actors</Link></li>
+          <li> <Link className="link-a" to='/actors'>Actors</Link></li>
           <li><input type="search" placeholder='Search...' className='input' /> </li>
         </ul>
-      </div>
+        </div>
       <div className="navbar-right">
         <div className="navbar-profile">
           <img src={profile_img} alt="profile" className='profile' />
@@ -31,7 +55,7 @@ const Navbar = () => {
         </div>
       </div>
     </div>
-  )
-}
-
-export default Navbar
+  );
+};
+export default Navbar;
+              
