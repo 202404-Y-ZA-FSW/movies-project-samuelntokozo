5b8875ec41168fa3466c52e700615365fefb090b
Ntokozo <nkosintokozot@gmail.com>
2024-06-24 15:15:41 +0200

    fixed nav

--- ntosamovies/src/components/Navbar/Navbar.jsx
+++ ntosamovies/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
-import profile_img from '../../assets/profile_img.png'
-import caret_icon from '../../assets/caret_icon.svg'
 import { Link } from 'react-router-dom';
-import { logout } from '../../Firebase'
+import {useState,useEffect,useNavigate} from 'react-router-dom'
+import axios from 'react-router-dom'
 
 
 
@@ -88,15 +87,7 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-      <div className="navbar-right">
-        <div className="navbar-profile">
-          <img src={profile_img} alt="profile" className='profile' />
-          <img src={caret_icon} alt="profile" />
-          <div className="dropdown">
-            <p onClick={()=>{logout()}}>Sign Out</p>
-          </div>
-        </div>
-      </div>
+     
 
     </div>
   );
