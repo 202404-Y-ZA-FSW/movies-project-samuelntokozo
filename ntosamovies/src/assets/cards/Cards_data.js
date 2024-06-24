d0a01db370c9b2f4b1c1bf7453307bd9845ff5b0
Thitevhelwi Samuel Masuvhe <31856641+samuelthis@users.noreply.github.com>
2024-06-24 12:40:10 +0200

    new

--- ntosamovies/src/assets/cards/Cards_data.js
+++ ntosamovies/src/assets/cards/Cards_data.js
@@ -1,17 +1,3 @@
-import card_img1 from './card1.jpg';
-import card_img2 from './card2.jpg';
-import card_img3 from './card3.jpg';
-import card_img4 from './card4.jpg';
-import card_img5 from './card5.jpg';
-import card_img6 from './card6.jpg';
-import card_img7 from './card7.jpg';
-import card_img8 from './card8.jpg';
-import card_img9 from './card9.jpg';
-import card_img10 from './card10.jpg';
-import card_img11 from './card11.jpg';
-import card_img12 from './card12.jpg';
-import card_img13 from './card13.jpg';
-import card_img14 from './card14.jpg';
 
 const cards_data = [
     {
